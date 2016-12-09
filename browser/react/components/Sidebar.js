7d0e193890d237e0c80ea9e2137d33676c630ad7
Beth Qiang <bethany.qiang@gmail.com>
2016-12-09 00:52:08 -0600

    Fix class of active sidebar links

--- browser/react/components/Sidebar.js
+++ browser/react/components/Sidebar.js
@@ -8,13 +8,13 @@ const Sidebar = () => {
     <sidebar>
       <img src="/juke.svg" className="logo" />
       <section>
-        <h4 className="menu-item active">
-          <Link to="/albums">ALBUMS</Link>
+        <h4 className="menu-item">
+          <Link to="/albums" activeClassName="active">ALBUMS</Link>
         </h4>
       </section>
       <section>
         <h4 className="menu-item">
-          <Link to="/artists">ARTISTS</Link>
+          <Link to="/artists" activeClassName="active">ARTISTS</Link>
         </h4>
       </section>
     </sidebar>
